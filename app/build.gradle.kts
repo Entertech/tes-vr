f6c908141d7336d659dc2bb18ac9c795e5136e33
wk <1226426603@qq.com>
2025-04-28 14:59:11 +0800

    0.0.9(9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.entertech.tes.vr"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
